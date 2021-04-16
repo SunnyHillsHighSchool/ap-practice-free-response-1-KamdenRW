71d37b181a55d00fd6c7c55918dabeccc6484cfc
Kamden Wright <>
2021-04-16 02:26:30 +0000

    Tested

--- Delimiters.java
+++ Delimiters.java
@@ -2,22 +2,43 @@ import java.util.*;
 
 public class Delimiters
 {
-	private String openDel;
-	private String closeDel;
+    private String openDel;
+    private String closeDel;
 
-	public Delimiters(String open, String close)
-	{
-		openDel = open;
-		closeDel = close;
-	}
+    public Delimiters(String open, String close)
+    {
+        openDel = open;
+        closeDel = close;
+    }
 
-	public ArrayList<String> getDelimitersList(String[] tokens)
-	{
-		return null;
-	}
+    public ArrayList<String> getDelimitersList(String[] tokens)
+    {
+      ArrayList<String> delimiters = new ArrayList<String>();
+      for(String t:tokens)
+      {
+       if(t.equals(openDel) || t.equals(closeDel))
+       {
+        delimiters.add(t);
+       }
+      }
+      return delimiters;
+    }
 
-	public boolean isBalanced(ArrayList<String> delimiters)
-	{ 
-		return false;
-	}
+    public boolean isBalanced(ArrayList<String> delimiters)
+    { 
+    int numOpen = 0, numClosed = 0;
+    for(String d:delimiters)
+    {
+      if(d.equals(openDel))
+      numOpen++;
+
+      if(d.equals(closeDel))
+      numClosed++;
+
+      if(numClosed > numOpen)
+      return false;
+    }
+
+        return numOpen == numClosed;
+    }
 }
